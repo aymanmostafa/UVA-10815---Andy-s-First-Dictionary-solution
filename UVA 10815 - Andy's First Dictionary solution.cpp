fa706b98dbc9dd90454137b9d40ea3a7833b8f37
Ayman Mostafa <ayman93live@hotmail.com>
2016-03-17 19:24:39 +0200

    Create UVA 10815 - Andy's First Dictionary solution.cpp

---
+++ UVA 10815 - Andy's First Dictionary solution.cpp
@@ -0,0 +1,72 @@
+//============================================================================
+// Name        : ayman.cpp
+// Author      : Ayman Mostafa
+// Email       : ayman93live@hotmail.com
+// Version     : v5.0
+//============================================================================
+
+#include <cstring>
+#include <map>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <complex>
+#include <list>
+#include <climits>
+#include <cctype>
+#include <bitset>
+
+using namespace std;
+
+#define all(x) x.begin(),x.end()
+#define allr(x) x.rbegin(),x.rend()
+
+void fastInOut();
+//##########################################################################################
+//###################################MAIN FUNCTION##########################################
+//##########################################################################################
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "rt", stdin);
+//freopen("output.txt", "wt", stdout);
+#endif
+	fastInOut();
+//----------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------
+// 10815 - Andy's First Dictionary
+	string s, d;
+	set < string > v;
+	while (cin >> s) {
+		d = "";
+		for (int i = 0; i < s.size(); i++)
+			if ((s[i] >= 65 && s[i] <= 90) || (s[i] >= 97 && s[i] <= 122))
+				d += tolower(s[i]);
+			else {
+				if (i) {
+					v.insert(d);
+					d = "";
+					s.erase(0, i);
+					i = -1;
+				}
+			}
+		v.insert(d);
+	}
+	for (auto i : v) {
+		if (i != "")
+			cout << i << endl;
+	}
+}
+void fastInOut() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL), cout.tie(NULL);
+}
